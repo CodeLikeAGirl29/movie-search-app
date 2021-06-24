145733670da8ea93811678b75b365d45c7b24883
Lindsey Howard <25946305+CodeLikeAGirl29@users.noreply.github.com>
2021-06-24 07:18:20 -0400

    added div to the h1 element

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ import ReactDOM from 'react-dom';
 class Main extends React.Component {
   render() {
     return (
+      <div class="container">
       <h1>Hello world!</h1>
+      </div>
     );
   }
 }
