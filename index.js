6aae9d5a515c12b2006403d228675c79128fa5aa
Lindsey Howard <25946305+CodeLikeAGirl29@users.noreply.github.com>
2021-06-24 07:22:11 -0400

    added searchMovies

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchMovies from "./searchMovies";
 
 class Main extends React.Component {
   render() {
     return (
       <div class="container">
-      <h1>Hello world!</h1>
+      <h1 class="title">Hello world!</h1>
+      <SearchMovies/>
       </div>
     );
   }
